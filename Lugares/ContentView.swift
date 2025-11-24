71565d0e277ba9aed11e9909d623f81de5a0ebbb
Alex Torres <alexander.torres.nce@gmail.com>
2025-11-24 13:39:49 -0600

    Add ContentView

---
+++ Lugares/ContentView.swift
@@ -0,0 +1,22 @@
+//  ContentView.swift
+//  Lugares
+//
+//  Created by SaucyBreeze under the NovaCloudEntertainment project.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
