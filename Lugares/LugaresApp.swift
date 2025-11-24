2728cf5d00421ca87b45231a614d4064c6851096
Alex Torres <alexander.torres.nce@gmail.com>
2025-11-24 13:51:14 -0600

    Add LugaresApp

---
+++ Lugares/LugaresApp.swift
@@ -0,0 +1,17 @@
+//  LugaresApp.swift
+//  Lugares
+//
+//  Created by Alex Torres on 05/21/2020.
+//  Revised by SaucyBreeze on 03/14/2024
+//  Restructured by SaucyBreeze on 04/07/2025 under the Nova Cloud Entertainment Project
+
+import SwiftUI
+
+@main
+struct LugaresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
