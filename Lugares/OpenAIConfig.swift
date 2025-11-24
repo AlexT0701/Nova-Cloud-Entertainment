a5218581511c2625cac00d30a3f7fc540d0616d7
Alex Torres <alexander.torres.nce@gmail.com>
2025-11-24 14:24:36 -0600

    Add OpenAIConfig

---
+++ Lugares/OpenAIConfig.swift
@@ -0,0 +1,10 @@
+//  OpenAIConfig.swift
+//  Lugares
+//
+//  This step is only used if the AI version is desired.
+//
+//  Created by SaucyBreeze under the Nova Cloud Entertainment project.
+
+struct OpenAIConfig {
+    static let apiKey = "YOUR_API_KEY_HERE"
+}
